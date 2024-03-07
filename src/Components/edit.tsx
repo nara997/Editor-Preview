30a8fe798b9a1b56deed071555e8c929af8a46ff
saikumar0262 <ksaikumar0262@gmail.com>
2024-03-07 18:07:50 +0530

    ts

--- src/Components/edit.tsx
+++ src/Components/edit.tsx
@@ -1,14 +1,12 @@
-
-import { useForm, Controller } from 'react-hook-form';
-
-
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+export type     IMessage = {
+  id: string;
+  subject: string;
+  body: string;
+};
 interface IData {
-    filterData: [{
-        id: string,
-        subject: string,
-        body: string
-    }],
-    setOpen: boolean,
+  filterData: IMessage[];
+  setOpen: (open: boolean) => any;
 }
 
 // const data: IData = {
@@ -17,101 +15,126 @@ interface IData {
 //     Body: "Greetings explorers! In our quest to unravel the mysteries of the quantum realm, let's delve into the mind-bending world of superposition and entanglement. Buckle up for a journey where particles can exist in multiple states simultaneously, and actions on one can instantaneously affect another, regardless of distance. Share your thoughts and theories as we navigate this perplexing yet fascinating domain",
 // };
 
-
 export const Editor = ({ filterData, setOpen }: IData) => {
+  const { handleSubmit, control } = useForm<IMessage>();
 
+  const onSubmit: SubmitHandler<IMessage> = (formData) => {
+    console.log("Form data:", formData);
+  };
 
-    const { handleSubmit, control } = useForm();
+  const cancelButton = () => {
+    setOpen(false);
+  };
 
-
-    const onSubmit = (formData: IData) => {
-
-
-        console.log('Form data:', formData);
-    };
-
-    const cancelButton = () => {
-        setOpen(false)
-    }
-
-    return (
-        <div className='p-9' >
-         
-
-            <h1 className='text-center font-semibold text-2xl '></h1>
-        <div className='justify-start flex p-2 mb-2' onClick={cancelButton}>
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" /> 
-            </svg>
-            <p className='pl-2'>Home</p>
-        </div>
-            <div className="container mx-auto max-w-7xl px-4 min-h-screen justify-around grid h-full gap-14 grid-flow-col grid-cols-2">
-                
-                {filterData.map((items) => (
-                    <>
-
-                        <div key={items.id} className='border border-slate-300 p-6 rounded-md shadow-md min-h-96 overscroll-contain overflow-auto h-48'>
-                            <div className='mb-4'>
-                                <h1 className='text-md font-semibold'>Subject:</h1>
-                                <p className='text-gray-700 text-sm font-normal' >{items.subject}</p>
-                            </div>
-                            <div className='flex flex-col'>
-                                <h1 className='text-md font-semibold'>Body:</h1>
-                                <p className='text-gray-700 flex-grow break-all text-sm font-normal'>{items.body}</p>
-                            </div>
-                        </div>
-                        {/* Editable Preview */}
-                        <div className='w-full  '>
-                            <form onSubmit={handleSubmit(onSubmit)} className='bg-white p-4 rounded-md shadow-md'>
-                                <div className='mb-4'>
-                                    <label htmlFor="subject" className='block text-md font-semibold text-gray-600'>Subject:</label>
-                                    <Controller
-                                        name="subject"
-                                        control={control}
-                                        defaultValue={items.subject}
-                                        render={({ field }) => (
-                                            <input
-                                                onChange={(e) => field.onChange(e.target.value)}
-                                                value={field.value}
-                                                type="text"
-                                                className='w-full px-3 py-2 text-sm border rounded-md focus:outline-none focus:border-blue-500 font-normal'
-                                                placeholder="Enter subject"
-                                            />
-                                        )}
-                                    />
-                                </div>
-                                <div className='mb-4'>
-                                    <label htmlFor="body" className='block text-md font-semibold text-gray-600'>Body:</label>
-                                    <Controller
-                                        name="body"
-                                        control={control}
-                                        defaultValue={items.body}
-                                        render={({ field }) => (
-                                            <textarea
-                                                onChange={(e) => field.onChange(e.target.value)}
-                                                value={field.value}
-                                                className='w-full min-h-52 px-3 py-2 text-sm font-normal border border-slate-300 rounded-md focus:outline-none focus:border-blue-500'
-                                                placeholder="Enter body"
-                                            />
-                                        )}
-                                    />
-                                </div>
-                                <div className='space-x-4'>
-
-                                    <button type="submit" className='bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300'>
-                                        Publish
-                                    </button>
-                                    <button type="submit" onClick={cancelButton} className='bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300'>
-                                        Back
-                                    </button>
-                                </div>
-                            </form>
-                        </div>
-                    </>
-                ))}
+  return (
+    <div className="p-9">
+      <h1 className="text-center font-semibold text-2xl "></h1>
+      <div className="justify-start flex p-2 mb-2" onClick={cancelButton}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M10 19l-7-7m0 0l7-7m-7 7h18"
+          />
+        </svg>
+        <p className="pl-2">Home</p>
+      </div>
+      <div className="container mx-auto max-w-7xl px-4 min-h-screen justify-around grid h-full gap-14 grid-flow-col grid-cols-2">
+        {filterData.map((items) => (
+          <>
+            <div
+              key={items.id}
+              className="border border-slate-300 p-6 rounded-md shadow-md min-h-96 overscroll-contain overflow-auto h-48"
+            >
+              <div className="mb-4">
+                <h1 className="text-md font-semibold">Subject:</h1>
+                <p className="text-gray-700 text-sm font-normal">
+                  {items.subject}
+                </p>
+              </div>
+              <div className="flex flex-col">
+                <h1 className="text-md font-semibold">Body:</h1>
+                <p className="text-gray-700 flex-grow break-all text-sm font-normal">
+                  {items.body}
+                </p>
+              </div>
             </div>
-        </div>
-    );
+            {/* Editable Preview */}
+            <div className="w-full  ">
+              <form
+                onSubmit={handleSubmit(onSubmit)}
+                className="bg-white p-4 rounded-md shadow-md"
+              >
+                <div className="mb-4">
+                  <label
+                    htmlFor="subject"
+                    className="block text-md font-semibold text-gray-600"
+                  >
+                    Subject:
+                  </label>
+                  <Controller
+                    name="subject"
+                    control={control}
+                    defaultValue={items.subject}
+                    render={({ field }) => (
+                      <input
+                        onChange={(e) => field.onChange(e.target.value)}
+                        value={field.value}
+                        type="text"
+                        className="w-full px-3 py-2 text-sm border rounded-md focus:outline-none focus:border-blue-500 font-normal"
+                        placeholder="Enter subject"
+                      />
+                    )}
+                  />
+                </div>
+                <div className="mb-4">
+                  <label
+                    htmlFor="body"
+                    className="block text-md font-semibold text-gray-600"
+                  >
+                    Body:
+                  </label>
+                  <Controller
+                    name="body"
+                    control={control}
+                    defaultValue={items.body}
+                    render={({ field }) => (
+                      <textarea
+                        onChange={(e) => field.onChange(e.target.value)}
+                        value={field.value}
+                        className="w-full min-h-52 px-3 py-2 text-sm font-normal border border-slate-300 rounded-md focus:outline-none focus:border-blue-500"
+                        placeholder="Enter body"
+                      />
+                    )}
+                  />
+                </div>
+                <div className="space-x-4">
+                  <button
+                    type="submit"
+                    className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
+                  >
+                    Publish
+                  </button>
+                  <button
+                    type="submit"
+                    onClick={cancelButton}
+                    className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
+                  >
+                    Back
+                  </button>
+                </div>
+              </form>
+            </div>
+          </>
+        ))}
+      </div>
+    </div>
+  );
 };
-
-// export default Editor;
--- src/Components/home.tsx
+++ src/Components/home.tsx
@@ -1,62 +1,78 @@
 // import React from 'react'
 
-import { useEffect, useState } from "react"
-import { Editor } from "./edit"
+import { useEffect, useState } from "react";
+import { Editor, IMessage } from "./edit";
 // import { useHistory } from 'react-router-dom';
 // import { Link } from "react-router-dom";
 
 
 
 export const Home = () => {
-    const [data, setData] = useState([])
-    const [filterData, setFilterData] = useState([])
-    const [open, setOpen] = useState(false)
+  const [data, setData] = useState<IMessage[]>([]);
+  const [filterData, setFilterData] = useState<IMessage[]>([]);
+  const [open, setOpen] = useState<boolean>(false);
 
-    // const history = useHistory();
+  // const history = useHistory();
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const response = await fetch("https://italent2.demo.lithium.com/api/2.0/search?q= select id,subject, body from messages where is_solution= true");
-            const jsonData = await response.json();
-            setData(jsonData.data.items)
-        }
-        fetchData()
-    }, [])
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://italent2.demo.lithium.com/api/2.0/search?q= select id,subject, body from messages where is_solution= true"
+      );
+      const jsonData = await response.json();
+      setData(jsonData.data.items);
+    };
+    fetchData();
+  }, []);
 
-    const buttonhandle = (id) => {
-        const filtered = data.filter(item => item.id === id);
-        setFilterData(filtered)
-        setOpen(true)
-    }
-    console.log(filterData, "filter")
+  const buttonhandle = (id: string) => {
+    const filtered = data.filter((item) => item.id === id);
+    setFilterData(filtered);
+    setOpen(true);
+  };
+  console.log(filterData, "filter");
 
-
-    return (
-        <div className="main-dev container max-2xl mx-auto" >
-             <div className=''>
-             <img src="https://www.italentdigital.com/wp-content/uploads/2022/08/italent-logo-black.svg" alt="italent-logo"/>
+  return (
+    <div className="main-dev container max-2xl mx-auto">
+      <div className="">
+        <img
+          src="https://www.italentdigital.com/wp-content/uploads/2022/08/italent-logo-black.svg"
+          alt="italent-logo"
+        />
+      </div>
+      {open === false && (
+        <div className="min-w-36">
+          <h1 className="text-center p-6 font-semibold text-2xl">
+            Message Moderator
+          </h1>
+          {data.map((items: any, index) => (
+            <div className="flex gap-5 flex-col w-[650px] mx-auto">
+              <div
+                key={index}
+                className="border-2 p-6 gap-3 rounded-md shadow-md min-h-11 flex justify-between h-24"
+              >
+                <p
+                  className="text-sm font-medium underline underline-offset-1 "
+                  onClick={() => buttonhandle(items.id)}
+                >
+                  {items.subject}
+                </p>
+                <button
+                  className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
+                  onClick={() => buttonhandle(items.id)}
+                >
+                  Moderate
+                </button>
+              </div>
             </div>
-            {open === false && 
-                <div className="min-w-36">
-                    <h1 className="text-center p-6 font-semibold text-2xl">Message Moderator</h1>
-                    {data.map((items: any, index) => (
-                        <div className="flex gap-5 flex-col w-[650px] mx-auto">
-
-                        <div key={index} className="border-2 p-6 gap-3 rounded-md shadow-md min-h-11 flex justify-between h-24">
-                            <p className="text-sm font-medium underline underline-offset-1 " onClick={() => buttonhandle(items.id)}>{items.subject}</p>
-                            <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300" onClick={() => buttonhandle(items.id)}>
-                             Moderate
-                            </button>
-                        </div>
-                        </div>
-                    ))}
-                </div>
-            }
-            {open === true && <div>
-                <Editor filterData={filterData} setOpen={setOpen} />
-            </div>
-
-            }
+          ))}
         </div>
-    )
-}
+      )}
+      {open && (
+        <div>
+          <Editor filterData={filterData} setOpen={setOpen} />
+        </div>
+      )}
+    </div>
+  );
+};
