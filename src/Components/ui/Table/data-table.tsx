1365d301de0282dcc4e5ef2de6582fe0c9197b67
saikumar0262 <ksaikumar0262@gmail.com>
2024-03-13 15:44:17 +0530

    pnpm issue fix

--- src/Components/ui/Table/data-table.tsx
+++ src/Components/ui/Table/data-table.tsx
@@ -32,6 +32,11 @@ export function DataTable<TData, TValue>({
     columns,
     getCoreRowModel: getCoreRowModel(),
   })
+
+
+
+
+
   return (
     <div className="rounded-md border">
       <Table>
